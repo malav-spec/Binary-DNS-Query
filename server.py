8729cc1fb4d8de7d369e64beedce870db069958f
Malav Doshi <56772095+malav-spec@users.noreply.github.com>
2021-03-11 05:58:39 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import binascii
 import math
+import sys
 
 def send_message(message, address, port):
 
@@ -16,6 +17,35 @@ def send_message(message, address, port):
     finally:
         ss.close()
     return binascii.hexlify(data).decode("utf-8")
+
+def connect_to_client(port):
+    list = []
+    sock_to_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('',port)
+    try:
+        sock_to_client.bind(server_address)
+        print("{S}: Socket create and bind successful")
+    except socket.error as err:
+        print("Error in socket bind")
+
+    sock_to_client.listen(1)
+
+    csockid, addr = sock_to_client.accept()
+
+    while True:
+        data = csockid.recv(1024).decode()
+        data = str(data)
+        ip = getIP("facebook.com")
+        # data = data.lower()
+        #print(data)
+
+        csockid.send(ip.encode('utf-8'))
+        if not data:
+            break
+        list.append(data)
+
+    sock_to_client.close()
+    return
 
 def format_list(elements):
     i = 1
@@ -37,49 +67,91 @@ def bin_to_ipv4(in_binary):
     format_bin = [x[::-1] for x in format_bin]
     format_bin.reverse()
     format_bin = [str(int(x,2)) for x in format_bin]
-    print(format_bin)
+    #print(format_bin)
     return format_list(format_bin)
 
 def getRequest(name):
     temp = name.split(".")
+    print(temp)
+    # host = temp[0]
+    # print(host)
+    # domain = temp[1]
+    # print(domain)
+    request = ""
 
-    host = temp[0]
-    domain = temp[1]
+    for i in range(len(temp)):
+        host = temp[i]
+        host_length = len(host)
 
-    host_length = len(host)
-    if host_length < 10:
-        host_length = "0" + str(host_length)
-        #host_length = int(host_length)
+        if host_length < 10:
+            host_length = "0" + str(host_length)
 
-    domain_length = len(domain)
-    if domain_length < 10:
-        domain_length = "0" + str(domain_length)
-        #domain_length = int(domain_length)
+        part = str(host_length)
+        request = request + part
 
-    request = str(host_length)
+        for j in range(int(host_length)):
+            print(host[j])
+            request = request  + " " + "".join(hex(ord(host[j]))[2:]) + " "
+        # request = request +  " "
 
-    for i in range(int(host_length)):
-        request = request + " " + "".join(hex(ord(host[i]))[2:])
-
-    request = request +  " " + str(domain_length)
-
-    for i in range(int(domain_length)):
-        request = request + " " + "".join(hex(ord(domain[i]))[2:])
+    #
+    # host_length = len(host)
+    # if host_length < 10:
+    #     host_length = "0" + str(host_length)
+    #     #host_length = int(host_length)
+    #
+    # domain_length = len(domain)
+    # if domain_length < 10:
+    #     domain_length = "0" + str(domain_length)
+    #     #domain_length = int(domain_length)
+    #
+    # request = str(host_length)
+    #
+    # for i in range(int(host_length)):
+    #     request = request + " " + "".join(hex(ord(host[i]))[2:])
+    #
+    # request = request +  " " + str(domain_length)
+    #
+    # for i in range(int(domain_length)):
+    #     request = request + " " + "".join(hex(ord(domain[i]))[2:])
 
     return request + " 00 00 01 00 01"
 
-header = "AA AA 01 00 00 01 00 00 00 00 00 00"
+def getIP(name):
+    header = "AA AA 01 00 00 01 00 00 00 00 00 00"
 
-#connect_to_client()
-request = getRequest("Example.com")
-message = header + " " + request
-message = message.replace(" ","").replace("\n","")
-response = send_message(message, "8.8.8.8", 53)
+    request = getRequest(name)
+    message = header + " " + request
+    message = message.replace(" ","").replace("\n","")
+    response = send_message(message, "8.8.8.8", 53)
 
-print(response)
-num = int(response, 16)
-print(num)
-ip = num & (pow(2,32) - 1)
-bin_ip = bin(ip).replace("0b", "")
-ip = bin_to_ipv4(bin_ip)
-print(ip)
+    print(response)
+    num = int(response, 16)
+    print(num)
+    ip = num & (pow(2,32) - 1)
+    print(ip)
+    print(ip.bit_length())
+    bin_ip = bin(ip).replace("0b", "")
+    ip = bin_to_ipv4(bin_ip)
+    print(ip)
+    return ip;
+
+
+
+# header = "AA AA 01 00 00 01 00 00 00 00 00 00"
+print(getIP("bbc.co.uk"))
+#connect_to_client(int(sys.argv[1]))
+#print(names_list)
+#
+# request = getRequest("google.com")
+# message = header + " " + request
+# message = message.replace(" ","").replace("\n","")
+# response = send_message(message, "8.8.8.8", 53)
+#
+# print(response)
+# num = int(response, 16)
+# print(num)
+# ip = num & (pow(2,32) - 1)
+# bin_ip = bin(ip).replace("0b", "")
+# ip = bin_to_ipv4(bin_ip)
+# print(ip)
